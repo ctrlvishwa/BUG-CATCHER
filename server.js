d5bdc916045d7710a4600d33ce711fe7fe227c7a
Nikhil GIRI <nikhilbaba15@gmail.com>
2026-07-30 04:05:22 +0530

    server.js

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.post('/api/analyze', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running securely on http://localhost:${PORT}`));
